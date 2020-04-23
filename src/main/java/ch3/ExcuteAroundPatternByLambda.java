5b54302b19fb18f5c2d5ed64e99a2c3e6257417c
hjmin <hjmin@rsupport.com>
2020-04-23 09:15:41 +0900

    lambda exam

---
+++ src/main/java/ch3/ExcuteAroundPatternByLambda.java
@@ -0,0 +1,38 @@
+package ch3;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.IO;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExcuteAroundPatternByLambda {
+
+  //실행 어라운드 패턴을 적용하는 네 단계의 과정
+
+  //1
+  public String processFile() throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+      return br.readLine();
+    }
+  }
+
+  //2
+  public interface BufferedReaderProcessor {
+    String process(BufferedReader br) throws IOException;
+  }
+
+  //3
+  public String processFile(BufferedReaderProcessor p) throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+      return p.process(br);
+    }
+  }
+
+  //4
+  public void runWithLambda() throws IOException{
+      String oneLine = processFile((BufferedReader br) -> br.readLine());
+      String twoLines = processFile((BufferedReader br) -> br.readLine() + br.readLine());
+  }
+
+
+}
