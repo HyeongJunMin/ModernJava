30c19ac4ce3cfdf109b87b087d73c491e1261a02
hjmin <hjmin@rsupport.com>
2020-05-12 19:33:37 +0900

    ch4 stream

--- src/test/java/ch4/InnerAndExternalLoop.java
+++ src/test/java/ch4/InnerAndExternalLoop.java
@@ -3,11 +3,13 @@ package ch4;
 import common.Dish;
 import common.Dish.Type;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -15,7 +17,7 @@ public class InnerAndExternalLoop {
 
   List<Dish> menu = new ArrayList();
 
-  @BeforeEach
+//  @BeforeAll
   void setUp() {
     menu.add(new Dish("pork", false, 800, Type.MEAT));
     menu.add(new Dish("beef", false, 700, Type.MEAT));
@@ -48,10 +50,30 @@ public class InnerAndExternalLoop {
     List<String> highCaloricDishes = menu.stream()
         .filter(dish -> dish.getCalories() > 300)
         .map(Dish::getName)
+//        .map(d -> d.getName())
         .collect(Collectors.toList());
 
     log.debug("highCaloricDishes : {}", highCaloricDishes);
     log.debug("highCaloricDishes size : {}", highCaloricDishes.size());
   }
 
+  @Test
+  public void oijdf() {
+    Date today = new Date(1588293000000L);
+    System.out.println("todat : " + today);
+    Date today2 = new Date(1588293000L);
+    System.out.println("today2 : " + today2);
+    Date now = new Date();
+
+
+    IntStream.range(0, 10).forEach(i -> {
+      if (i == 4)
+        return;
+      log.debug("i : {}",i);
+    });
+  }
+
+  private boolean isExtendableDate(Date endDate) {
+    return (endDate.getTime() > 253402268399000L) ? false : true;
+  }
 }
